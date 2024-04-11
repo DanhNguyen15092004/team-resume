510f6e90e20bf77844edff8dda5d0cdbbffff2e0
DorothyTom <thyntt10112002@gmail.com>
2024-04-11 21:22:55 +0700

    add Components

---
+++ src/Components/Content/Content.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Content.css'
+import LeftContent from '../LeftContent/LeftContent'
+
+const Content = () => {
+  return (
+    <div className="app">
+        <div className='content'>
+        <div className="left-content">
+            <LeftContent />
+        </div>
+       
+    </div>
+    </div>
+  )
+}
+
+export default Content
