cbe325ee4e457d3459584126183edb88a59a3838
DorothyTom <thyntt10112002@gmail.com>
2024-04-11 21:22:55 +0700

    add Components

---
+++ src/Components/LeftContent/LeftContent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './LeftContent.css'
+import Image from '../Image/Image'
+
+const LeftContent = () => {
+  return (
+    <div className='left-content'>
+      <Image />
+    </div>
+  )
+}
+
+export default LeftContent
