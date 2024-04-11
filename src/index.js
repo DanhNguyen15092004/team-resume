6869469cdf0186c530a461e29643540a1f46e3b3
thy-nguyen10 <94232298+thy-nguyen10@users.noreply.github.com>
2024-04-11 22:23:17 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import Content from './Components/Content/Content';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
     <Content/>
   </React.StrictMode>
 );
