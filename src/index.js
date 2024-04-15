69739f3d943795aa8394d39d46018f4cc6f8ecca
WannaSu1cide <ngdanh1509@gmail.com>
2024-04-15 13:16:03 +0700

    add import info

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import Content from './Components/Content/Content';
+import Info from './Components/Info/Info';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
