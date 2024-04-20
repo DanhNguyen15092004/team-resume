ad771d0671267abe66885b12e7904a8632a71674
WannaSu1cide <ngdanh1509@gmail.com>
2024-04-20 16:02:08 +0700

    implement Technology

--- src/Components/RightContent/Body/Body.jsx
+++ src/Components/RightContent/Body/Body.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import './Body.css'; 
 import Certificate from "./Certificate/Certificate";
+import Technology from "./Technology/Technology";
 const Body = () => {
     return ( 
         <div className="body">
-            <div className="certificate">
+            <div className="Experience">
+                <h1 className="experience-title">
+                    WORK EXPERIENCE
+                </h1>
+                <hr/>
                 <Certificate/>
+                <Technology/>
             </div>
         </div>
     )
