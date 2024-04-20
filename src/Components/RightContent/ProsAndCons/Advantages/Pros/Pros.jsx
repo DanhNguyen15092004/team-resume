9fbf6b0a68bd96567ffafaf97af16550cb3195fd
WannaSu1cide <ngdanh1509@gmail.com>
2024-04-20 16:20:33 +0700

    implement prosAndCons

---
+++ src/Components/RightContent/ProsAndCons/Advantages/Pros/Pros.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Pros = () => { 
+    <div className="pros-contaner">
+        <p className="text">
+            Good communicate with teammate
+        </p>
+        <p className="text">
+            Want to learn more and more 
+        </p> 
+        <p className="text">
+            Self taught
+        </p> 
+    </div>
+}
+export default Pros
---
+++ src/Components/RightContent/ProsAndCons/Advantages/ProsAndCons.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import Pros from "./Pros/Pros";
+
+const ProsAndCons = () => { 
+    <Pros/>
+}
+export default ProsAndCons
